abe7a2433534fcdd2352b8f06c93fcff340977d1
alinacode17 <alinawebdeveloper@gmail.com>
2021-03-30 22:04:03 +0100

    1 file modified

--- src/ItemPicker.js
+++ src/ItemPicker.js
@@ -6,7 +6,7 @@ export default function ItemPicker() {
   return (
     <>
       <NumberSelector initialNumber={3} title={"Bed"} />
-      <NumberSelector initialNumber={2} title={"Table"} />
+      <NumberSelector initialNumber={2} title={"Small Table"} />
       <NumberSelector initialNumber={0} title={"Armchair"} />
     </>
   );
