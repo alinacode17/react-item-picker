1f21aba0a6d6bc9dc7adf84a1f3a6889eebbe9b2
alinacode17 <alinawebdeveloper@gmail.com>
2021-03-30 22:53:19 +0100

    added data array and looped through data to render items

--- src/ItemPicker.js
+++ src/ItemPicker.js
@@ -3,11 +3,33 @@ import "./style.css";
 import NumberSelector from "./NumberSelector";
 
 export default function ItemPicker() {
+  let data = [
+    {
+      id: 1,
+      itemName: "Bed",
+      initialNumber: 2
+    },
+    {
+      id: 2,
+      itemName: "Armchair",
+      initialNumber: 5
+    },
+    {
+      id: 3,
+      itemName: "Table",
+      initialNumber: 0
+    }
+  ];
+
   return (
-    <>
-      <NumberSelector initialNumber={3} title={"Bed"} />
-      <NumberSelector initialNumber={2} title={"Small Table"} />
-      <NumberSelector initialNumber={0} title={"Armchair"} />
-    </>
+    <div className="item-picker">
+      {data.map(item => (
+        <NumberSelector
+          initialNumber={item.initialNumber}
+          title={item.itemName}
+          key={item.id}
+        />
+      ))}
+    </div>
   );
 }
